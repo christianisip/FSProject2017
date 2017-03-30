e0210a5f906b154e2f540b3e199fa54f616928ad
Christian Isip <Christian.i09@hotmail.com>
2017-03-30 18:29:40 +0000

    Added validation and partials view

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :category
   mount_uploader :image, ImageUploader
+
+  validates :name, :price, presence: true
+  validates :mfr, uniqueness: true
 end
