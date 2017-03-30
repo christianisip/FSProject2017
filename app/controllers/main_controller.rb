88a814e986407c45d513d6ed12411ab528d68153
Christian Isip <Christian.i09@hotmail.com>
2017-03-30 16:09:13 +0000

    Added search category to filter products

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,17 +5,21 @@ class MainController < ApplicationController
       @today = Product.where("created_at >= ?", Time.zone.now.beginning_of_day)
 
       if params[:search]
-            @product = Product.where("name LIKE '%#{params[:search]}%' OR description LIKE '%#{params[:search]}%'")
-      elsif params[:category_id]
-        @product = Product.where("category_id = #{ params[:category_id] }")
+        # @product = Product.where("name LIKE '%#{params[:search]}%' OR description LIKE '%#{params[:search]}%'").page(params[:page]).per(2)
+        if params[:category_id] == "all"
+        # @product = Product.where("category_id = #{ params[:category_id] }").page(params[:page]).per(2)
+          @product = Product.where("name LIKE '%#{params[:search]}%' OR description LIKE '%#{params[:search]}%'").page(params[:page]).per(2)
+        else
+          @product = Product.where("name LIKE '%#{params[:search]}%' AND category_id LIKE '%#{params[:category_id]}%'").page(params[:page]).per(2)
+        end
       else
         @product = Product.all.page(params[:page]).per(2)
       end
-    end
+  end
 
     def display
-    @category = Category.all
-    @product = Product.find(params[:number].to_i)
+      @category = Category.all
+      @product = Product.find(params[:number].to_i)
   end
 
 end
