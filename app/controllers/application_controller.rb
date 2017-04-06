780407a934e30c4389bf6293b8ad276893457ab8
christianisip <Christian.i09@hotmail.com>
2017-04-06 02:14:25 -0500

    Added flash message when item has been succesfully deleted from the cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def flash_class(level)
+    case level
+        when :notice then "alert alert-info"
+        when :success then "alert alert-success"
+        when :error then "alert alert-error"
+        when :alert then "alert alert-error"
+    end
+  end
+
 end
