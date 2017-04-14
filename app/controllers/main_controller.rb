b263ba8eb661f328892167a5695dc7e7c4bb579b
christianisip <christian.i09@hotmail.com>
2017-04-14 05:53:15 +0000

    Rubocop fixes and css heading styling

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,117 +1,101 @@
 class MainController < ApplicationController
-before_action :initialize_session
+  before_action :initialize_session
 
-def index
-  @all = User.all
-  @testProduct = Product.all
-  if params[:search]
-    # @product = Product.where("name LIKE '%#{params[:search]}%' OR description LIKE '%#{params[:search]}%'").page(params[:page]).per(2)
-    if params[:category_id] == "all"
-    # @product = Product.where("category_id = #{ params[:category_id] }").page(params[:page]).per(2)
-      @product = Product.where("name LIKE '%#{params[:search]}%' OR description LIKE '%#{params[:search]}%'").order(:name).page(params[:page]).per(2)
+  def index
+    if params[:search]
+      if params[:category_id] == 'all'
+        @product = Product.where("name LIKE '%#{params[:search]}%' OR description LIKE
+                                  '%#{params[:search]}%'").order(:name).page(params[:page]).per(2)
+      else
+        @product = Product.where("name LIKE '%#{params[:search]}%' AND category_id
+                                  LIKE '%#{params[:category_id]}%'").page(params[:page]).per(1)
+      end
+    elsif params[:category_type]
+      @product = Product.where("category_id =
+      #{params[:category_type]}").order(:name).page(params[:page]).per(3)
     else
-      @product = Product.where("name LIKE '%#{params[:search]}%' AND category_id LIKE '%#{params[:category_id]}%'").page(params[:page]).per(1)
+      @product = Product.all.page(params[:page]).per(3)
     end
-  elsif params[:category_type]
-    @product = Product.where("category_id = #{params[:category_type]}").order(:name).page(params[:page]).per(3)
-  else
-    @product = Product.all.page(params[:page]).per(3)
-  end
-end
-
-def display
-  @category = Category.all
-  @product = Product.find(params[:number].to_i)
-end
-
-def newproduct
-  @newproduct = Product.where(
-      'created_at >= :five_days_ago',
-      :five_days_ago  => Time.now - 4.days
-  ).page(params[:page]).per(3)
-  @recentlyproduct = Product.where(
-      'updated_at >= :five_days_ago',
-      :five_days_ago  => Time.now - 5.days
-  ).page(params[:page]).per(3)
-end
-
-def recentlyproduct
-  @newproduct = Product.where(
-      'created_at >= :five_days_ago',
-      :five_days_ago  => Time.now - 4.days
-  ).page(params[:page]).per(3)
-  @recentlyproduct = Product.where(
-      'updated_at >= :five_days_ago',
-      :five_days_ago  => Time.now - 5.days
-  ).page(params[:page]).per(3)
-end
-
-def login
-  if params[:username]
-    User.create!(
-      username: params[:username], firstname: params[:firstname],
-      lastname: params[:lastname], password: params[:password], address: params[:address],
-      province_id: params[:province_id], email: params[:email], phonenumber: params[:phonenumber]
-                )
   end
 
-  if params[:userlogin]
-    @user = User.find_by("username IS '#{params[:usernamelogin]}'")
+  def display
+    @category = Category.all
+    @product = Product.find(params[:number].to_i)
+  end
+
+  def newproduct
+    @newproduct = Product.where(
+      'created_at >= :five_days_ago', five_days_ago: Time.current - 4.days
+    ).page(params[:page]).per(3)
+  end
+
+  def recentlyproduct
+    @recentlyproduct = Product.where(
+      'updated_at >= :five_days_ago', five_days_ago: Time.current - 5.days
+    ).page(params[:page]).per(3)
+  end
+
+  def login
+    if params[:username]
+      User.create!(
+        username: params[:username], firstname: params[:firstname],
+        lastname: params[:lastname], password: params[:password], address: params[:address],
+        province_id: params[:province_id], email: params[:email], phonenumber: params[:phonenumber]
+      )
+    end
+
+    return unless params[:userlogin]
+    @user = User.find_by("username IS '#{params[:userlogin]}'")
     if @user && params[:userlogin] == @user.username && params[:passlogin] == @user.password
       session[:user] = @user.username
       session[:province] = @user.province.name
       session[:pst] = @user.province.pst
       session[:gst] = @user.province.gst
-
-      redirect_to index_path
     else
-      redirect_to cart_path
+      redirect_to index_path
     end
   end
-end
 
-def cart
-  @addcart = Product.find(session[:to_cart_list])
-  @qtyproduct = params[:quantity]
-  session[:totalprice] = 0
-end
+  def cart
+    @addcart = Product.find(session[:to_cart_list])
+    @qtyproduct = params[:quantity]
+    session[:totalprice] = 0
+  end
 
-def remember_to_cart
-  id = params[:id].to_i
-  session[:to_cart_list] << id unless session[:to_cart_list].include?(id)
-  redirect_back(fallback_location: cart_path)
-end
+  def remember_to_cart
+    id = params[:id].to_i
+    session[:to_cart_list] << id unless session[:to_cart_list].include?(id)
+    redirect_back(fallback_location: cart_path)
+  end
 
-def delete_to_cart
-  id = params[:id].to_i
-  session[:to_cart_list].delete(id)
-  redirect_back(fallback_location: cart_path)
-  flash[:notice] = 'Delete successfully'
-end
+  def delete_to_cart
+    id = params[:id].to_i
+    session[:to_cart_list].delete(id)
+    redirect_back(fallback_location: cart_path)
+    flash[:notice] = 'Delete successfully'
+  end
 
-def contact
-  @contact = Information.all
-end
+  def contact
+    @contact = Information.all
+  end
 
-def about
-  @about = Information.all
-end
+  def about
+    @about = Information.all
+  end
 
   private
 
-def initialize_session
-  session[:to_cart_list] ||= []
-end
+  def initialize_session
+    session[:to_cart_list] ||= []
+  end
 
-def initilize_user_info
-  session[:user] = @user.username
-  session[:province] = @user.province.name
-  session[:pst] = @user.province.pst
-  session[:gst] = @user.province.gst
-end
-  #
-  # def products_to_call
-  #   Product.find(session[:to_cart_list])
-  # end
-  # helper_method :products_to_call
+  def initilize_user_info
+    session[:user] = @user.username
+    session[:province] = @user.province.name
+    session[:pst] = @user.province.pst
+    session[:gst] = @user.province.gst
+  end
+
+  def test
+  end
 end
